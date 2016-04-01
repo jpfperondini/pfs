53019700b05bef538fb86f3dc3a9f465c351e8a4
João Pedro Perondini <jpfperondini@gmail.com>
2016-04-01 17:00:58 -0300

    Add statement item value validation

--- statement.py
+++ statement.py
@@ -1,4 +1,4 @@
-import csv, sqlite3
+import csv
 
 def importfile(conn, path):
     c = conn.cursor()
@@ -6,6 +6,17 @@ def importfile(conn, path):
     with open(path, 'rb') as csvfile:
         rdr = csv.reader(csvfile, delimiter=';', quotechar='|')
         for row in rdr:
-            c.execute('insert into financial_transaction values (?, ?, ?, ?)', [int(row[3]), row[0], row[2], row[4]])
+            value = _check_value(row[4]).replace(',', '')
+            c.execute('insert into financial_transaction values (?, ?, ?, ?)', [int(row[3]), row[0], row[2], value])
     conn.commit()
     c.close()
+
+def _check_value(value):
+    comma_idx = value.find(',')
+    if comma_idx == -1:
+        raise ValueError("Invalid input " + value)
+
+    if len(value) - comma_idx < 3:
+        raise ValueError("Invalid input " + value)
+
+    return value;
--- test_statement.py
+++ test_statement.py
@@ -2,17 +2,37 @@ import unittest, statement, db, logging
 
 class TestStatement(unittest.TestCase):
 
-  def test_import_file(self):
-      conn = db.init_memory()
-      expected1 = (123456, "01/01/2016", "A", u'120,21')
-      expected2 =  (654321, "02/01/2016","A","-54,31")
-      statement.importfile(conn, "tests/statement.csv")
-      c = conn.cursor()
-      c.execute("select * from financial_transaction")
-      self.assertEqual(expected1,  c.fetchone())
-      self.assertEqual(expected2,  c.fetchone())
-      c.close
-      conn.close
+    def _test_check_value_for_invalid_input(self, value):
+        conn = db.init_memory()
+        self.assertRaises(ValueError, statement._check_value, value)
+        conn.close
+
+    def test_import_file(self):
+        conn = db.init_memory()
+        expected1 = (123456, "01/01/2016", "A", u'12021')
+        expected2 =  (654321, "02/01/2016","A","-5431")
+        statement.importfile(conn, "tests/statement.csv")
+        c = conn.cursor()
+        c.execute("select * from financial_transaction")
+        self.assertEqual(expected1,  c.fetchone())
+        self.assertEqual(expected2,  c.fetchone())
+        c.close
+        conn.close
+
+    def test_check_value(self):
+        conn = db.init_memory()
+        value = '123,12'
+        self.assertEqual(value, statement._check_value(value))
+        conn.close
+
+    def test_check_value_for_scale_1(self):
+        self._test_check_value_for_invalid_input('123,1')
+
+    def test_check_value_for_scale_0(self):
+        self._test_check_value_for_invalid_input('123,')
+
+    def test_check_value_for_scale(self):
+        self._test_check_value_for_invalid_input('123')
 
 if __name__ == '__main__':
     unittest.main()
